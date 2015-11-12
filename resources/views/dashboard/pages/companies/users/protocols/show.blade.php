20ed4282fd82107a99f69681032d583e0a6832d3
Luis Felipe Izquierdo <felipeizquierdoo@gmail.com>
2015-11-12 20:59:39 +0000

    ajustando enlace presentar examen

--- resources/views/dashboard/pages/companies/users/protocols/show.blade.php
+++ resources/views/dashboard/pages/companies/users/protocols/show.blade.php
@@ -42,8 +42,32 @@
                             @foreach($protocol->links as $link)
                             <li title="{{$link->description}}">
                                 <div class="row">
-                                    <div class="col-xs-8">
-                                        <h4 style="font-size:16px;"><a title="{{ $link->description }}" href="{{ $link->url }}" target="_blank">{{ $link->name }}</a></h4>
+                                    <div class="col-xs-12">
+                                        <h4 style="font-size:16px;"><i class="fa fa-external-link fa-fw"></i><a href="{{ $link->url }}" target="_blank">{{ $link->name }}</a></h4>
+                                    </div>
+                                </div>
+                            </li>
+                            @endforeach
+                        </ul>
+                    </div>
+                </div>
+            </div>
+
+            <div class="row">
+                <div class="block">
+                    <div class="block-title">
+                        <h2>Anexos</h2>
+                    </div>
+                    <div class="block-section">
+                        <ul class="list-unstyled">
+                            @foreach($protocol->getAnnexes() as $annex)
+                            <li>
+                                <div class="row">
+                                    <div class="col-xs-12">
+                                        <h4>
+                                            <i class="fa fa-file-photo-o"></i>
+                                            <a href="/storage/{{$annex}}" target="_blank">{{ explode('/', $annex)[3] }}</a>
+                                        </h4>
                                     </div>
                                 </div>
                             </li>
@@ -54,37 +78,23 @@
             </div>
 
 			<div class="row">
-				<div class="block">
-					<div class="block-title">
-						<div class="block-options pull-right">
-						</div>
-						<h2>{{$protocol->number_annex}} Anexos </h2>
-					</div>
-					<div class="block-section">
-						<ul class="fa-ul">
-
-		                </ul>
-					</div>
-				</div>
-			</div>
-			<div class="row">
 
 					<a href=" {{route('exams.create', $protocol->id)}}" class="widget" title="Presentar Examen">
 
 					<a href="#" class="widget" title="Examen no disponible">
 
 					<div class="widget-content widget-content-mini themed-background-muted text-center">
-						<i class="fa fa-bar-chart-o"></i> @if($user->best_exam_score > 0) Mejorar Calificación @else Presentar Examen @endif
+						<i class="fa fa-bar-chart-o"></i> @if($protocol->bestExam($user)->score > 0) Mejorar Calificación @else Presentar Examen @endif
 					</div>
 					<div class="widget-content text-center">
-						<div class="pie-chart easyPieChart" data-percent="{{$user->best_exam_score}}" data-line-width="3" data-bar-color="#cccccc" data-track-color="#f9f9f9" style="width: 80px; height: 80px; line-height: 80px;">
-							<span><strong>{{$user->best_exam_score}}%</strong></span>
+						<div class="pie-chart easyPieChart" data-percent="{{ $protocol->bestExam($user)->score }}" data-line-width="3" data-bar-color="#cccccc" data-track-color="#f9f9f9" style="width: 80px; height: 80px; line-height: 80px;">
+							<span><strong>{{ $protocol->bestExam($user)->score }}%</strong></span>
 							<canvas width="80" height="80"></canvas>
 						</div>
 					</div>
 					<div class="widget-content themed-background-muted">
 						<div class="progress progress-striped progress-mini active remove-margin">
-							<div class="progress-bar progress-bar-danger" role="progressbar" aria-valuenow="{{$user->best_exam_score}}" aria-valuemin="0" aria-valuemax="100" style="width: {{$user->best_exam_score}}%"></div>
+							<div class="progress-bar progress-bar-danger" role="progressbar" aria-valuenow="{{ $protocol->bestExam($user)->score }}" aria-valuemin="0" aria-valuemax="100" style="width: {{ $protocol->bestExam($user)->score }}%"></div>
 						</div>
 					</div>
 				</a>
